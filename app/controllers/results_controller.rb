c482d17e2b80737676991d2479c57714ffafe919
jdmcafee24 <jdmcafee24@yahoo.com>
2019-11-12 05:28:26 +0000

    included price range in user's preferences

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,16 +4,21 @@
 class ResultsController < ApplicationController
 
     def index 
-        @cat = [params[:categories].join(' ')]
-        restaurants = Client.search(@cat)
+        @cat = params[:categories]? [params[:categories].join(' ')] : ['food']
+        @prices = params[:prices]? [params[:prices].join(',')] : ['1,2,3,4']
+        restaurants = Client.search(@cat, @prices)
         @restaurants = restaurants["businesses"].uniq {|biz| biz["name"] } unless restaurants == nil
     end
 
     def create
-        if params[:restCategory]
+        if params[:restCategory] && params[:restPrice]
+            redirect_to results_path(categories: params[:restCategory], prices: params[:restPrice])
+        elsif params[:restCategory]
             redirect_to results_path(categories: params[:restCategory])
+        elsif params[:restPrice]
+            redirect_to results_path(prices: params[:restPrice])
         else
-            redirect_to results_path(categories: ['food'])
+            redirect_to results_path()
         end
     end
 end
